156aac4ec51b3ad45636aed9909c34aa90a1a828
Erick Duarte <erickod@gmail.com>
2023-03-30 12:23:29 -0300

    feat: add NotificationFactoryCaller

---
+++ system_notification/domain/notification_factory_caller.py
@@ -0,0 +1,32 @@
+from typing import Dict, Literal
+
+from system_notification.domain.notifications.notification_target import (
+    NotificationTarget,
+)
+from system_notification.domain.protocols.notification_factory_protocol import (
+    NotificationFactory,
+)
+from system_notification.domain.protocols.notification_protocol import Notification
+from system_notification.domain.protocols.notification_sender import NotificationSender
+
+
+class NotificationFactoryCaller:
+    def __init__(self) -> None:
+        self._factories: Dict[str, NotificationFactory] = {}
+
+    def add_factory(self, factory: NotificationFactory) -> None:
+        self._factories[factory.target_type] = factory
+
+    async def get_sender(self, target: NotificationTarget) -> NotificationSender | None:
+        factory = self._factories[target.type]
+        return factory.make_sender()
+
+    async def get_notification(
+        self,
+        title: str,
+        content: str,
+        destin: NotificationTarget,
+        priority: Literal[0, 1, 2, 3] = 0,
+    ) -> Notification | None:
+        factory = self._factories[destin.type]
+        return factory.make_notificaton(title, content, priority)
