8a0e21d5f6e58436260b2450a0180535e1f24049
Erick Duarte <erickod@gmail.com>
2023-03-30 13:28:50 -0300

    feat: add controller protocol

---
+++ system_notification/domain/protocols/controller_protocol.py
@@ -0,0 +1,22 @@
+from typing import Protocol
+
+from system_notification.infra.http.server.helpers.http_request import HttpRequest
+from system_notification.infra.http.server.helpers.http_response import HttpResponse
+
+
+class HttpServer(Protocol):
+    def serve(self, port: int = 8000) -> None:
+        ...
+
+    def on(self, method: str, url: str, controller: "Controller") -> None:
+        ...
+
+
+class Controller(Protocol):
+    http_server: HttpServer
+
+    def __init__(self, http_server: HttpServer) -> None:
+        ...
+
+    def handle(self, request: HttpRequest) -> HttpResponse:
+        ...
