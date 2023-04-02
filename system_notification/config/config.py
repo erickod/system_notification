87ac9cd21ea64c3691de7622def6d15ad38f9300
Erick Duarte <erickod@gmail.com>
2023-04-02 07:58:09 -0300

    feat: add config pkg

---
+++ system_notification/config/config.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+from dynaconf import Dynaconf
+
+settings = Dynaconf(
+    settings_files=["settings.toml", ".secrets.toml"],
+    envvar_prefix=False,
+)
+ENVIRON_TYPE = settings.get("environ_type", "dev")
+SETTINGS = settings.get(ENVIRON_TYPE) or defaultdict(lambda: "missing data")
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load these files in the order.
